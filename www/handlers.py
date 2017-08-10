8c8b9f5ab0cca285cc57c775a4c556b6389d1c85
Zuoweixiang <445923239@qq.com>
2017-08-10 18:55:35 +0800

    commit publish blog
    publish blog

--- www/handlers.py
+++ www/handlers.py
@@ -141,5 +141,26 @@ def authenticate(*,email,passwd):
 
 
 
+#blogs
+@get('/api/blogs/{id}')
+def api_get_blog(*,id):
+    blog = Blog.find(id)
+    return blog
+
+@post('/api/blogs/create')
+def api_create_blog(request,*,name,summary,content):
+    #check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name','name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary','summary cannot be empty.')
+    if not  content or not content.strip():
+        raise APIValueError('conten','content cannot be empty.')
+    blog = Blog(user_id = request.__user__.id,user_name = request.__user__.name,user_image=request.__user__.image,name =name.strip(),content=content.strip())
+    yield from blog.save()
+    return blog
+
+
+
 
 
